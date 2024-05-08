2033d5145420419cbde12f42fb35696bfe4bfc24
Er4ld0 <junior-eraldo2011@hotmail.com>
2024-05-08 05:24:07 -0300

    Ajuste de JS

---
+++ js/index.js
@@ -0,0 +1,96 @@
+function flipCard() {
+    if (card.style.transform == 'rotateY(180deg)') {
+        card.style.transform = 'none'
+    }
+    else {
+        card.style.transform = 'rotateY(180deg)'
+    }
+}
+function flipCard1() {
+    if (card1.style.transform == 'rotateY(180deg)') {
+        card1.style.transform = 'none'
+    }
+    else {
+        card1.style.transform = 'rotateY(180deg)'
+    }
+}
+function flipCard2() {
+    if (card2.style.transform == 'rotateY(180deg)') {
+        card2.style.transform = 'none'
+    }
+    else {
+        card2.style.transform = 'rotateY(180deg)'
+    }
+}
+function flipCard3() {
+    if (card3.style.transform == 'rotateY(180deg)') {
+        card3.style.transform = 'none'
+    }
+    else {
+        card3.style.transform = 'rotateY(180deg)'
+    }
+}
+function flipCard4() {
+    if (card4.style.transform == 'rotateY(180deg)') {
+        card4.style.transform = 'none'
+    }
+    else {
+        card4.style.transform = 'rotateY(180deg)'
+    }
+}
+function flipCard5() {
+    if (card5.style.transform == 'rotateY(180deg)') {
+        card5.style.transform = 'none'
+    }
+    else {
+        card5.style.transform = 'rotateY(180deg)'
+    }
+}
+function flipCard6() {
+    if (card6.style.transform == 'rotateY(180deg)') {
+        card6.style.transform = 'none'
+    }
+    else {
+        card6.style.transform = 'rotateY(180deg)'
+    }
+}
+function flipCard7() {
+    if (card7.style.transform == 'rotateY(180deg)') {
+        card7.style.transform = 'none'
+    }
+    else {
+        card7.style.transform = 'rotateY(180deg)'
+    }
+}
+function flipCard8() {
+    if (card8.style.transform == 'rotateY(180deg)') {
+        card8.style.transform = 'none'
+    }
+    else {
+        card8.style.transform = 'rotateY(180deg)'
+    }
+}
+function flipCard9() {
+    if (card9.style.transform == 'rotateY(180deg)') {
+        card9.style.transform = 'none'
+    }
+    else {
+        card9.style.transform = 'rotateY(180deg)'
+    }
+}
+function flipCard10() {
+    if (card10.style.transform == 'rotateY(180deg)') {
+        card10.style.transform = 'none'
+    }
+    else {
+        card10.style.transform = 'rotateY(180deg)'
+    }
+}
+function flipCard11() {
+    if (card11.style.transform == 'rotateY(180deg)') {
+        card11.style.transform = 'none'
+    }
+    else {
+        card11.style.transform = 'rotateY(180deg)'
+    }
+}
